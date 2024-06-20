ca1b26d980ec8b0f5e9a1d5a4da15b7d7a075031
seanchang <molybdenite@gmail.com>
2024-06-20 09:25:49 +0800

    EOS2 rotated during the Hualien earthquake on April 3, 2024.

--- EOS/EOS_rotate.py
+++ EOS/EOS_rotate.py
@@ -6,7 +6,7 @@ import numpy.linalg as LA
 class OBS_rotate():
     """
     This class rotate CWB EOS stations from input stream and return a new stream stored rotated station with new channel id.
-    
+
     Program will automatic trim and fill_value for input stream to rotate.
 
     Make sure input EOS traces with the same starttime and endtime.
@@ -14,7 +14,7 @@ class OBS_rotate():
     def __init__(self, OriDat="obs_ori.dat"):
         # path to orientation info file
         self.OriDat = OriDat
-    
+
     def data_pre_process(self, st):
         tmp_st = st.select(network='TW', location="20").copy()
         # check EOS station
@@ -34,7 +34,7 @@ class OBS_rotate():
         trim_edtime = edtime[0]
         tmp_st.trim(trim_sttime, trim_edtime)
         return tmp_st
-    
+
     def rotate_EOS_station(self, st):
         # set all possible channel pair in EOS2~EOSA
         CHAN_PAIR = (['HL1','HL2','HL3'], ['HN1','HN2','HN3'], ['EH1','EH2','EH3'], ['HA1','HA2','HA3'])
@@ -55,7 +55,7 @@ class OBS_rotate():
                 EOS_st = Stream()
                 for CHN in PAIR:
                     EOS_st += tmp_st.select(station=STN).select(channel=CHN)
-                
+
                 if len(EOS_st) == 3:
                     PHI = np.deg2rad(self.STNs_Roll[STN])
                     PSI = np.deg2rad(self.STNs_Pitch[STN])
@@ -69,7 +69,7 @@ class OBS_rotate():
                     # combine to output stream
                     new_EOS_st += EOS_st
         return new_EOS_st
-    
+
     def make_YAW_PITCH_ROLL(self, x_vector, y_vector, z_vector):
         """
         calculate rotate degree from input x, y, z vector (mean of daily seismogram).
@@ -78,37 +78,37 @@ class OBS_rotate():
         PHI = np.arctan2(y_vector, z_vector)
         PSI = np.arctan2(-x_vector, YZ_len)
         return PHI, PSI
-    
+
     def make_rotate_matrix(self, PHI, PSI, azimuth):
         """
         pre-multiply three rotation matrix for rotate seismogram
         """
         COS_PHI = np.cos(-PHI)
         SIN_PHI = np.sin(-PHI)
-        
+
         ROTATEM_PHI = np.array([[1.0, 0.0, 0.0], \
                             [0.0, COS_PHI,  SIN_PHI], \
                             [0.0, -SIN_PHI,  COS_PHI]])
-        
+
         COS_PSI = np.cos(-PSI)
         SIN_PSI = np.sin(-PSI)
-        
+
         ROTATEM_PSI = np.array([[ COS_PSI, 0.0, -SIN_PSI], \
                                 [ 0.0, 1.0, 0.0], \
                                 [ SIN_PSI, 0.0, COS_PSI]])
-        
-        
+
+
         COS_AZI = np.cos(np.deg2rad(azimuth))
         SIN_AZI = np.sin(np.deg2rad(azimuth))
-        
+
         ROTATEM_azimuth = np.array([[ COS_AZI, SIN_AZI, 0.0], \
                                     [-SIN_AZI, COS_AZI, 0.0], \
                                     [     0.0,     0.0, 1.0]])
-        
+
         ROTATE_M_TMP = np.matmul(ROTATEM_PSI, ROTATEM_PHI)
         ROTATE_M = np.matmul(ROTATEM_azimuth, ROTATE_M_TMP)
         return ROTATE_M
-    
+
     def rotate_seis(self, st, PHI, PSI, azimuth):
         """
         rotate seismogram
@@ -122,9 +122,9 @@ class OBS_rotate():
         st[2].data = SEIS_NEW[0]
         st[1].data = SEIS_NEW[1]
         st[0].data = SEIS_NEW[2]
-        
+
         return
-        
+
     def get_station_info(self):
         """
         get station orientation info from input file.
@@ -135,12 +135,12 @@ class OBS_rotate():
         self.STNs_Roll   = {}
         self.STNs_Pitch  = {}
         self.STNs_Az     = {}
-        
+
         for line in ORI_DAT:
             STN = line[0:5].strip()
             period_start = int(line[37:45])
             period_stop  = int(line[46:54])
-            
+
             if STN not in self.STNs:
                 if TIME >= period_start and TIME <= period_stop:
                     self.STNs.append(STN)
